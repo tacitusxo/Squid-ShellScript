3172b2dcac88a45790647601556af83c4171d00c
KotaroTaki <38860388+takikunxo@users.noreply.github.com>
2024-05-26 19:51:40 +0900

    Update CentOS7-BasicAuthentication.sh

--- CentOS7-BasicAuthentication.sh
+++ CentOS7-BasicAuthentication.sh
@@ -1,36 +1,39 @@
 #!/bin/sh
+#パッケージのアンインストール
+sudo yum -y remove squid
+sudo rm -f /etc/squid/squid.conf
 #パッケージのインストール
 sudo yum -y install squid
 sudo yum -y install httpd
 sudo yum -y install expect
 #ポート開放
-sudo firewall-cmd --zone=public --add-port=userport/tcp --permanent
+sudo firewall-cmd --zone=public --add-port=3128/tcp --permanent
 sudo firewall-cmd --reload
 #認証用ユーザーの作成
 ID="userid"
 PW="userpass"
 expect -c "
 set timeout 20
-spawn htpasswd -c /etc/squid/.htpasswd \"${ID}\n\"
+spawn htpasswd -c /etc/squid/.htpasswd \"${ID}\"
 expect \"password:\"
 send \"${PW}\n\"
 expect \"password:\"
 send \"${PW}\n\"
 expect \"$\"  exit 0" 
 #プロキシサーバー経由での接続を隠蔽 
-sudo sed -i -e '/acl CONNECT method CONNECT/ a\forwarded_for off' /etc/squid/squid.conf
-sudo sed -i -e '/acl CONNECT method CONNECT/ a\request_header_access X-Forwarded-For deny all' /etc/squid/squid.conf
-sudo sed -i -e '/acl CONNECT method CONNECT/ a\request_header_access Via deny all' /etc/squid/squid.conf
-sudo sed -i -e '/acl CONNECT method CONNECT/ a\request_header_access Cache-Control deny all' /etc/squid/squid.conf
-sudo sed -i -e '/acl CONNECT method CONNECT/ a\no_cache deny all' /etc/squid/squid.conf
+sudo sed -i -e '/http_access allow localhost manager/ a\forwarded_for off' /etc/squid/squid.conf
+sudo sed -i -e '/http_access allow localhost manager/ a\request_header_access X-Forwarded-For deny all' /etc/squid/squid.conf
+sudo sed -i -e '/http_access allow localhost manager/ a\request_header_access Via deny all' /etc/squid/squid.conf
+sudo sed -i -e '/http_access allow localhost manager/ a\request_header_access Cache-Control deny all' /etc/squid/squid.conf
+sudo sed -i -e '/http_access allow localhost manager/ a\no_cache deny all' /etc/squid/squid.conf
 #Basic認証用の設定
-sudo sed -i -e '/acl CONNECT method CONNECT/ a\http_access allow password' /etc/squid/squid.conf
-sudo sed -i -e '/acl CONNECT method CONNECT/ a\acl password proxy_auth REQUIRED' /etc/squid/squid.conf
-sudo sed -i -e '/acl CONNECT method CONNECT/ a\auth_param basic program /usr/lib64/squid/basic_ncsa_auth /etc/squid/.htpasswd' /etc/squid/squid.conf
-sudo sed -i -e '/acl CONNECT method CONNECT/ a\auth_param basic children 5' /etc/squid/squid.conf
-sudo sed -i -e '/acl CONNECT method CONNECT/ a\auth_param basic realm Basic Authentication' /etc/squid/squid.conf
-sudo sed -i -e '/acl CONNECT method CONNECT/ a\auth_param basic credentialsttl 24 hours' /etc/squid/squid.conf
+sudo sed -i -e '/http_access allow localhost manager/ a\http_access allow password' /etc/squid/squid.conf
+sudo sed -i -e '/http_access allow localhost manager/ a\acl password proxy_auth REQUIRED' /etc/squid/squid.conf
+sudo sed -i -e '/http_access allow localhost manager/ a\auth_param basic program /usr/lib64/squid/basic_ncsa_auth /etc/squid/.htpasswd' /etc/squid/squid.conf
+sudo sed -i -e '/http_access allow localhost manager/ a\auth_param basic children 5' /etc/squid/squid.conf
+sudo sed -i -e '/http_access allow localhost manager/ a\auth_param basic realm Basic Authentication' /etc/squid/squid.conf
+sudo sed -i -e '/http_access allow localhost manager/ a\auth_param basic credentialsttl 24 hours' /etc/squid/squid.conf
 #サービス起動設定
 sudo systemctl enable squid
-sudo systemctl start squid
+sudo systemctl restart squid
 sudo systemctl status squid.service
